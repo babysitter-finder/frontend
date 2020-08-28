7dd7b4cde38d0541eb019415945bf254664398e9
Abdiel Ortega <nathanabdiel1@gmail.com>
2020-08-28 16:16:46 -0500

    feature/stars The stars to get rank were programed

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Router, Route, Switch } from 'react-router-dom';
-import Button from '../components/atoms/Button';
 import { createBrowserHistory } from 'history';
 import '../styles/main.scss';
+import StarsRate from '../components/atoms/StarsRate';
+import StarsRating from '../components/atoms/StarsRating';
 
 const App = () => {
   return (
     <Router history={ createBrowserHistory() }>
       <Switch>
         <Route exact path="/">
-          <Button text="Type me" className="button-pink" />
+          <StarsRating />
         </Route>
       </Switch>
     </Router>

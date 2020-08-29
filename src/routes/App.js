488ad959e74eda58c4e01c39b2fd9509c0cc57ec
Abdiel Ortega <nathanabdiel1@gmail.com>
2020-08-29 11:54:33 -0500

    feature/availability-input This commit has the component to check availability #18

--- src/routes/App.js
+++ src/routes/App.js
@@ -3,13 +3,15 @@ import { Router, Route, Switch } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
 import '../styles/main.scss';
 import Header from '../components/organisms/Header';
+import AvailabilityInput from '../components/molecules/AvailabilityInput';
 
 const App = () => {
   return (
     <Router history={ createBrowserHistory() }>
       <Switch>
-        <Header />
+        {/* <Header /> */}
         <Route exact path="/">
+          <AvailabilityInput mini />
         </Route>
       </Switch>
     </Router>

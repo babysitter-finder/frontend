0ccce137d2c13e847156820f494b64ccfa3e4d7d
Abdiel Ortega <nathanabdiel1@gmail.com>
2020-09-10 03:12:16 -0500

    feature/profile-logic The profile logic was developed

--- src/containers/clients/Profile.jsx
+++ src/containers/clients/Profile.jsx
@@ -2,11 +2,21 @@ import React, { useState } from 'react';
 import photo from '../../assets/girl.jpeg';
 import { Link } from 'react-router-dom';
 import PopupDeleteUser from '../../components/molecules/PopupDeleteUser';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import differenceInYears from 'date-fns/differenceInYears';
 
-const Profile = () => {
+const Profile = ({ user }) => {
+
+
   const [isOpenPopup, setIsOpenPopup] = useState(false);
   const handleClose = () => {
     setIsOpenPopup(false);
+  };
+
+  const genres = {
+    male: 'Masculino',
+    female: 'Femenino'
   };
 
   return (
@@ -16,16 +26,16 @@ const Profile = () => {
         <div className="profile-content">
           <div className="profile-divide">
             <div className="profile-imgContainer">
-              <img src={ photo } alt="" />
+              <img src={ user?.picture ?? photo } alt="" />
             </div>
             <div className="profile-data">
-              <h3>Nombre: Nathan Socorro</h3>
-              <h3>Edad: 28</h3>
-              <h3>Genero: Masculino</h3>
-              <h3>Celular: 442678373</h3>
+              <h3>Nombre: { `${user.first_name} ${user.last_name}` }</h3>
+              <h3>Edad: { differenceInYears(Date.now(), new Date(user.birthdate)) ?? '28' }</h3>
+              <h3>Genero: { genres[user.genre] ?? 'Masculino'}</h3>
+              <h3>Celular: { user.phone_number ?? '442678373'}</h3>
             </div>
           </div>
-          <h3>Direccion: Primera, Constitucion 325  Morelos Aguascalientes, Ags.</h3>
+          <h3>Direccion: { user.address ?? 'Primera, Constitucion 325  Morelos Aguascalientes, Ags.'}</h3>
           <div className="buttons">
             <button className="button-pink" onClick={ () => setIsOpenPopup(true) }>Eliminar cuenta</button>
             {isOpenPopup && <PopupDeleteUser closePopup={ handleClose } />}
@@ -37,4 +47,12 @@ const Profile = () => {
   );
 };
 
-export default Profile;
+Profile.propTypes = {
+  user: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (reducer) => {
+  return reducer.usersReducer;
+};
+
+export default connect(mapStateToProps)(Profile);
